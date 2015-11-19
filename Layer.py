8f029650bd5d36b6f7eb173dc1d7fe06f17a3aa0
Davinellulinvega <casparluc@gmail.com>
2015-11-19 15:22:39 +0000

    Add the modules required to build a simple BP neural network

---
+++ Layer.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+__author__ = 'davinellulinvega'
+
+#Import the required packages
+import Synapse
+import Neuron
+
+
+#Define the Layer class
+class Layer:
+	"""The basic collection of computing unit in the neural network"""
+
+	def __init__(self, nb_neuron, prev_layer=None):
+		"""Define the attributes and initialize their value"""
+
+		#A list of all the neurons in the layer
+		self.neurons = []
+
+		#Create nb_neuron new neurons
+		while nb_neuron > 0:
+			#Create the neuron
+			tmp_neuron = Neuron.Neuron()
+			#Add the neuron to the layer's list
+			self.neurons.append(tmp_neuron)
+			#Decrease the number of neuron
+			nb_neuron -= 1
+
+		#Check if the previous layer exist
+		if not(prev_layer is None) and isinstance(prev_layer, Layer):
+			#For each neuron in the layer
+			for tmp_neuron in self.neurons:
+				#For each neuron in the previous layer
+				for prev_neuron in prev_layer.neurons:
+					#Create a synapse
+					tmp_syn = Synapse.Synapse(prev_neuron, tmp_neuron)
+					#Add the synapse in the out list
+					prev_neuron.add_out_syn(tmp_syn)
+					#Add the synapse in the in list
+					tmp_neuron.add_in_syn(tmp_syn)
+
+	def update_weight(self):
+		"""Define the procedure to update the weights of all incoming synapses"""
+
+		#For each neuron in the layer
+		for neuron in self.neurons:
+			#Ask the neuron to update the incoming synaptic weight
+			neuron.update_weight()
+
+	def update_error(self, error=None):
+		"""Define the procedure to update the weights of all neurons in the layer"""
+
+		#For each neuron in the layer
+		if error is None:
+			for index, neuron in enumerate(self.neurons):
+				#Ask the neuron to update its error
+				neuron.update_error()
+		else:
+			for index, neuron in enumerate(self.neurons):
+				#Ask the neuron to update its error
+				neuron.update_error(error[index])
+
+	def activate(self):
+		"""Define the procedure to activate all neurons in the layer"""
+
+		#For each neuron in the layer
+		for neuron in self.neurons:
+			#Ask the neuron to activate
+			neuron.activate()
---
+++ Neuron.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python
+__author__ = 'davinellulinvega'
+
+# Import the required packages
+from math import tanh
+import Synapse
+
+
+# Declare the class Neuron
+class Neuron:
+	"""The basic computing unit on the neural network"""
+
+	def __init__(self):
+		"""Define the attributes and initialize the neuron"""
+
+		self.output = 0
+		self.in_syn = []
+		self.out_syn = []
+		self.error = 0
+
+	def add_in_syn(self, synapse):
+		"""Define the operations to be executed when adding an input synapse"""
+
+		#Check if the synapse is an instance of Synapse
+		if not(isinstance(synapse, Synapse.Synapse)):
+			#If not raise an error
+			raise TypeError("Only synapses can link neurons")
+		else:
+			#Else add the synapse to the in_syn list
+			self.in_syn.append(synapse)
+
+	def add_out_syn(self, synapse):
+		"""Define the operations to be executed when adding an ouput synapse"""
+
+		#Check if the synapse is an instance of Synapse
+		if not(isinstance(synapse, Synapse.Synapse)):
+			#If not raise an error
+			raise TypeError("Only synapses can link neurons")
+		else:
+			#Else add the synapse to the out_syn list
+			self.out_syn.append(synapse)
+
+	def update_error(self, error=None):
+		"""Define the procedure to update the error on the output"""
+
+		#If no error is provided
+		if error is None:
+			sum_err = 0
+			#For each out synapse
+			for syn in self.out_syn:
+				sum_err += syn.neuron_out.error * syn.weight
+			#Compute the error
+			self.error = sum_err * (1 - self.output**2)
+		else:
+			#Else store the error value
+			self.error = error
+
+	def update_weight(self):
+		"""Define the procedure to update the weight of the input synapses"""
+
+		#For each input synapse
+		for syn in self.in_syn:
+			#Ask the synapse to update its weight
+			syn.update_weight()
+		#Normalize the synaptic weight
+		#self.normalize_weight()
+
+	def normalize_weight(self):
+		"""Normalize the weight, while keeping the dispersion ratio"""
+
+		#For each input synapse
+		avg = 0
+		#Compute the average
+		for syn in self.in_syn:
+			avg += syn.weight
+		avg /= len(self.in_syn)
+
+		#For each input synapse
+		var = 0
+		#Compute the standard deviation
+		for syn in self.in_syn:
+			var += (syn.weight - avg)**2
+		var /= len(self.in_syn)
+		dev = var**0.5
+
+		#For each input synapse
+		for syn in self.in_syn:
+			#Substract the average
+			syn.weight -= avg
+			#And divide by the standard deviation
+			syn.weight /= dev
+
+	def activate(self):
+		"""Compute the new value of the neuron's output"""
+
+		#Get the weighted sum of the inputs
+		sum_in = 0
+		for syn in self.in_syn:
+			sum_in += syn.get_weighted_input()
+
+		#Apply the activation function and assign the result to the output
+		self.output = tanh(sum_in)
