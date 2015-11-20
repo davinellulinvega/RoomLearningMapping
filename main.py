d16a07f5d972faf886d1f0e271a73997b6e03cc9
Davinellulinvega <casparluc@gmail.com>
2015-11-20 15:52:31 +0000

    Implement the standard procedure for the beginning and the end of the program

--- main.py
+++ main.py
@@ -3,3 +3,23 @@
 __author__ = 'davinellulinvega'
 
 import Sphero
+
+# Instantiate a Sphero object
+sphero = Sphero.Sphero(True, [10, 10], [10, 10])
+
+# Connect to the robot
+sphero.connect()
+
+# Configure the robot
+sphero.configure()
+
+# Start a second thread for processing async data
+sphero.start()
+
+try:
+
+except KeyboardInterrupt:
+    # Shut sphero down
+    sphero.shutdown()
+    # Exit the program with a little message
+    print("Goodbye !!! Hope to see you again soon!!!")
