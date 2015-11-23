4878bcac7d3911de63dc6eaa171c79da297017c0
Davinellulinvega <casparluc@gmail.com>
2015-11-23 18:21:10 +0000

    Reformat source code

--- Neuron.py
+++ Neuron.py
@@ -21,73 +21,73 @@ class Neuron:
     def add_in_syn(self, synapse):
         """Define the operations to be executed when adding an input synapse"""
 
-        #Check if the synapse is an instance of Synapse
-        if not(isinstance(synapse, Synapse.Synapse)):
-            #If not raise an error
+        # Check if the synapse is an instance of Synapse
+        if not (isinstance(synapse, Synapse.Synapse)):
+            # If not raise an error
             raise TypeError("Only synapses can link neurons")
         else:
-            #Else add the synapse to the in_syn list
+            # Else add the synapse to the in_syn list
             self.in_syn.append(synapse)
 
     def add_out_syn(self, synapse):
         """Define the operations to be executed when adding an ouput synapse"""
 
-        #Check if the synapse is an instance of Synapse
-        if not(isinstance(synapse, Synapse.Synapse)):
-            #If not raise an error
+        # Check if the synapse is an instance of Synapse
+        if not (isinstance(synapse, Synapse.Synapse)):
+            # If not raise an error
             raise TypeError("Only synapses can link neurons")
         else:
-            #Else add the synapse to the out_syn list
+            # Else add the synapse to the out_syn list
             self.out_syn.append(synapse)
 
     def update_error(self, error=None):
         """Define the procedure to update the error on the output"""
 
-        #If no error is provided
+        # If no error is provided
         if error is None:
             sum_err = 0
-            #For each out synapse
+            # For each out synapse
             for syn in self.out_syn:
                 sum_err += syn.neuron_out.error * syn.weight
-            #Compute the error
-            self.error = sum_err * (1 - self.output**2)
+            # Compute the error
+            self.error = sum_err * (1 - self.output ** 2)
         else:
-            #Else store the error value
+            # Else store the error value
             self.error = error
 
     def update_weight(self):
         """Define the procedure to update the weight of the input synapses"""
 
-        #For each input synapse
+        # For each input synapse
         for syn in self.in_syn:
-            #Ask the synapse to update its weight
+            # Ask the synapse to update its weight
             syn.update_weight()
-        #Normalize the synaptic weight
-        #self.normalize_weight()
+            # Normalize the synaptic weight
+            # self.normalize_weight()
 
     def normalize_weight(self):
         """Normalize the weight, while keeping the dispersion ratio"""
 
-        #For each input synapse
+        # For each input synapse
         avg = 0
-        #Compute the average
+        # Compute the average
         for syn in self.in_syn:
             avg += syn.weight
         avg /= len(self.in_syn)
 
-        #For each input synapse
+        # For each input synapse
         var = 0
-        #Compute the standard deviation
+        # Compute the standard deviation
         for syn in self.in_syn:
-            var += (syn.weight - avg)**2
+            var += (syn.weight - avg) ** 2
         var /= len(self.in_syn)
-        dev = var**0.5
+        dev = var ** 0.5
 
-        #For each input synapse
+        # For each input synapse
         for syn in self.in_syn:
-            #Substract the average
+            # Subtract the average
             syn.weight -= avg
-            #And divide by the standard deviation
+            # And divide by the standard deviation
             syn.weight /= dev
 
     def activate(self):
--- Synapse.py
+++ Synapse.py
@@ -6,45 +6,45 @@ from random import uniform
 import Neuron
 
 
-#Define the Synapse class
+# Define the Synapse class
 class Synapse:
-	"""The basic connection between computing units in the neural network"""
+    """The basic connection between computing units in the neural network"""
 
-	def __init__(self, neuron_in, neuron_out):
-		"""Define the synapse's attribute and initialize their value"""
+    def __init__(self, neuron_in, neuron_out):
+        """Define the synapse's attribute and initialize their value"""
 
-		#Initialize the weight to a random number between 0 and 1
-		self.weight = uniform(-1, 1)
-		#Keep a record of the previous delta, to compute the momentum
-		self.prev_delta = 0
-		self.eta = 0.01  # 0.0001
-		self.epsylon = 0.7
-		#Check if neuron_in is an instance of Neuron
-		if not(isinstance(neuron_in, Neuron.Neuron)):
-			#If not raise an error
-			raise TypeError("Only neurons can be connected to a synapse")
-		else:
-			#Else store the instance
-			self.neuron_in = neuron_in
-		#Check if neuron_out is an instance of Neuron
-		if not(isinstance(neuron_out, Neuron.Neuron)):
-			#If not raise an error
-			raise TypeError("Only neurons can be connected to a synapse")
-		else:
-			#Else store the instance
-			self.neuron_out = neuron_out
+        # Initialize the weight to a random number between 0 and 1
+        self.weight = uniform(-1, 1)
+        # Keep a record of the previous delta, to compute the momentum
+        self.prev_delta = 0
+        self.eta = 0.01  # 0.0001
+        self.epsylon = 0.7
+        # Check if neuron_in is an instance of Neuron
+        if not (isinstance(neuron_in, Neuron.Neuron)):
+            # If not raise an error
+            raise TypeError("Only neurons can be connected to a synapse")
+        else:
+            # Else store the instance
+            self.neuron_in = neuron_in
+            # Check if neuron_out is an instance of Neuron
+        if not (isinstance(neuron_out, Neuron.Neuron)):
+            # If not raise an error
+            raise TypeError("Only neurons can be connected to a synapse")
+        else:
+            # Else store the instance
+            self.neuron_out = neuron_out
 
-	def update_weight(self):
-		"""Define the procedure for updating the weight of the synapse"""
+    def update_weight(self):
+        """Define the procedure for updating the weight of the synapse"""
 
-		#Compute the delta
-		delta = self.eta * self.neuron_out.error * self.neuron_in.output + self.epsylon * self.prev_delta
-		#Compute the new weight
-		self.weight += delta
-		#Store the delta in the previous delta
-		self.prev_delta = delta
+        # Compute the delta
+        delta = self.eta * self.neuron_out.error * self.neuron_in.output + self.epsylon * self.prev_delta
+        # Compute the new weight
+        self.weight += delta
+        # Store the delta in the previous delta
+        self.prev_delta = delta
 
-	def get_weighted_input(self):
-		"""Returns the value of the neuron_in multiplied by the weight of the synapse"""
+    def get_weighted_input(self):
+        """Returns the value of the neuron_in multiplied by the weight of the synapse"""
 
-		return self.weight * self.neuron_in.output
+        return self.weight * self.neuron_in.output
