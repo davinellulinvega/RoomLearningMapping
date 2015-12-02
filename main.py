0e61e4b83223107700aeb7ddfd011835dec0586d
Davinellulinvega <casparluc@gmail.com>
2015-12-02 12:16:02 +0000

    Remove a print introduced for debugging purpose

--- main.py
+++ main.py
@@ -28,8 +28,6 @@ try:
         # Get the parameters for the next roll
         speed, heading = sphero.get_roll_params(0xff)
 
-        print("Action: (S, H) ({}, {})".format(speed, heading))
-
         # Roll according to the parameters
         sphero.roll(speed, heading, 0x01, False)  # The third parameter is the state: 0x00->breaking, 0x01->driving
 
